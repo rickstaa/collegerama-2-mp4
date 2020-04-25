74222b1151429b1614773b2e482df74b7002a6b9
Rick Staa <rick.staa@outlook.com>
2020-04-25 19:07:00 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Collegerama2mp4
+
+A small python script I used to download videos from collegerama so I could watch them
+off-line.
---
+++ get_lec.py
@@ -0,0 +1,30 @@
+# Import Modules
+import webbrowser
+from time import sleep# this should go at the top of the file
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+# Get user input
+print('-- Get lecture script --')
+url_lec = input('Give url: ')
+
+# Get rendered HTML
+driver = webdriver.Chrome("/home/username/ChromeDriver/chrome‌​driver")
+driver.set_window_position(-10000,0)
+driver.get(url_lec)
+sleep(4)
+html = driver.page_source
+driver.quit()
+
+# Extract values with beautifulsoup
+soup = BeautifulSoup(html, "html.parser")
+print('')
+print("The video stream urls are:")
+url = []
+for tags in soup.find_all('video'):
+  url.append(tags.source['src'])
+  print("-", tags.source['src'])
+
+# Download video
+webbrowser.open_new_tab(url[0])
+webbrowser.open_new_tab(url[1])
